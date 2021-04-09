48a0dd313565b48fb738f765aff8893e2664f03b
isabella-cruz-ifood <isabella.cruz@ifood.com.br>
2021-04-09 11:28:20 -0300

    Implementando search view e trazendo localizacao

--- app/src/main/java/com/isabella/nearbypet/MapsActivity.kt
+++ app/src/main/java/com/isabella/nearbypet/MapsActivity.kt
@@ -3,10 +3,17 @@ package com.isabella.nearbypet
 import android.Manifest
 import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.SearchView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
@@ -23,40 +30,88 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.LocationSettingsRequest
+import java.io.IOException
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var searchView: SearchView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        checkPermission()
+
+        searchView = findViewById(R.id.search_view_map)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        // adding on query listener for our search view.
+        searchView!!.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // on below line we are getting the
+                // location name from search view.
+                val location = searchView!!.query.toString()
 
+                // below line is to create a list of address
+                // where we will store the list of all address.
+                var addressList: List<Address>? = null
 
+                // checking if the entered location is null or not.
+                if (location != null || location == "") {
+                    // on below line we are creating and initializing a geo coder.
+                    val geocoder = Geocoder(this@MapsActivity)
+                    try {
+                        // on below line we are getting location from the
+                        // location name and adding that location to address list.
+                        addressList = geocoder.getFromLocationName(location, 1)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                    // on below line we are getting the location
+                    // from our list a first position.
+                    val address: Address = addressList!![0]
+
+                    // on below line we are creating a variable for our location
+                    // where we will add our locations latitude and longitude.
+                    val latLng = LatLng(address.getLatitude(), address.getLongitude())
+
+                    // on below line we are adding marker to that position.
+                    mMap!!.addMarker(MarkerOptions().position(latLng).title(location))
+
+                    // below line is to animate camera to that position.
+                    mMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        // at last we calling our map fragment to update.
+        mapFragment.getMapAsync(this)
+
+        checkPermission()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val nearByPet = LatLng(-23.6930654, -46.6144001)
-        mMap.addMarker(MarkerOptions().position(nearByPet).title("Near by Pet Test"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(nearByPet))
+//        // Add a marker in Sydney and move the camera
+//        val nearByPet = LatLng(-23.6930654, -46.6144001)
+//        mMap.addMarker(MarkerOptions().position(nearByPet).title("Near by Pet Test"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(nearByPet))
+//        mMap.isMyLocationEnabled
+//        mMap.uiSettings.isMyLocationButtonEnabled = false
+//
 
     }
 
@@ -95,6 +150,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 }
+
+
+
+
+
+
+
 
 
 

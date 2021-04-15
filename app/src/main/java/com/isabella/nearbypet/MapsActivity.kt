aa331683093f60c6ae92b2df7cb4ceaef51e00c9
isabella-cruz-ifood <isabella.cruz@ifood.com.br>
2021-04-15 18:17:55 -0300

    Adicionando marker a localizacao atual

--- app/src/main/java/com/isabella/nearbypet/MapsActivity.kt
+++ app/src/main/java/com/isabella/nearbypet/MapsActivity.kt
@@ -1,48 +1,48 @@
 package com.isabella.nearbypet
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
-import android.view.KeyEvent
-import android.view.inputmethod.EditorInfo
-import android.widget.EditText
 import android.widget.SearchView
-import android.widget.TextView
 import android.widget.Toast
-import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
-
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.maps.model.*
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import java.io.IOException
 
 
+@Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS", "DEPRECATED_IDENTITY_EQUALS")
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var searchView: SearchView
+    private lateinit var client: FusedLocationProviderClient
+
+
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        client = LocationServices.getFusedLocationProviderClient(this)
 
         searchView = findViewById(R.id.search_view_map)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -67,7 +67,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     try {
                         // on below line we are getting location from the
                         // location name and adding that location to address list.
-                        addressList = geocoder.getFromLocationName(location, 1)
+                        addressList = geocoder.getFromLocationName(location, 5)
                     } catch (e: IOException) {
                         e.printStackTrace()
                     }
@@ -81,9 +81,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
                     // on below line we are adding marker to that position.
                     mMap!!.addMarker(MarkerOptions().position(latLng).title(location))
+                        .setIcon(BitmapDescriptorFactory.defaultMarker(
+                            BitmapDescriptorFactory.HUE_VIOLET))
 
                     // below line is to animate camera to that position.
+                    mMap!!.animateCamera(CameraUpdateFactory.newLatLng(latLng))
                     mMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+
                 }
                 return false
             }
@@ -98,22 +102,94 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         checkPermission()
     }
 
+    @SuppressLint("MissingPermission")
+    override fun onResume() {
+        super.onResume()
+//    Capture last location of device
+        client.lastLocation
+            .addOnSuccessListener { location ->
+                if (location != null) {
+                    val latLng = LatLng(location.latitude, location.longitude)
+                    mMap!!.addMarker(MarkerOptions().position(latLng).title(location.toString()))
+                        .setIcon(BitmapDescriptorFactory.defaultMarker(
+                            BitmapDescriptorFactory.HUE_VIOLET))
+                    mMap!!.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+                    mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+                } else {
+                    Log.i("Teste",
+                        "null")
+                }
+            }
+            .addOnFailureListener { }
 
+//      Setting the interval that my request will capture the location of the user
+//      Setting the locations of the others apps to my app consume (nearby locations)
+//      Setting the precision of my location and putting the economy of my battery.
+        val locationRequest : LocationRequest = LocationRequest.create()
+        locationRequest.interval = 15 * 1000
+        locationRequest.fastestInterval = 5 * 1000
+        locationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
 
+        val builder : LocationSettingsRequest.Builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+//      Checking if my configs is correct
+        val settingsClient = LocationServices.getSettingsClient(this)
+        settingsClient.checkLocationSettings(builder.build())
+            .addOnSuccessListener { locationResponse ->
+                Log.i("Teste",
+                    locationResponse.locationSettingsStates.isNetworkLocationPresent.toString())
+            }
+            .addOnFailureListener { e ->
+                if (e is ResolvableApiException) {
+                    try {
+                        val resolvable: ResolvableApiException = e
+                        resolvable.startResolutionForResult(this@MapsActivity, 10)
+                    } catch (e1: IntentSender.SendIntentException) {
+                    }
+                }
+            }
+
+        val locationCallback: LocationCallback = object : LocationCallback() {
+//          Capture current location after 15sec
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                    if (locationResult == null) {
+                        Log.i("Teste2", "Local is null")
+                        return
+                    }
+                    for (location: Location in locationResult.locations) {
+                        Log.i("Teste2", location.latitude.toString())
+                    }
+            }
+
+            override fun onLocationAvailability(locationAvailability: LocationAvailability) {
+                super.onLocationAvailability(locationAvailability)
+                Log.i("Teste", locationAvailability.isLocationAvailable.toString())
+            }
+        }
+
+        client.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
 
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED) {
+            checkPermission()
+            return
+        }
+        mMap.isMyLocationEnabled = true
+        mMap.uiSettings.isMyLocationButtonEnabled = true
 
-//        // Add a marker in Sydney and move the camera
-//        val nearByPet = LatLng(-23.6930654, -46.6144001)
-//        mMap.addMarker(MarkerOptions().position(nearByPet).title("Near by Pet Test"))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(nearByPet))
-//        mMap.isMyLocationEnabled
-//        mMap.uiSettings.isMyLocationButtonEnabled = false
-//
+        }
 
-    }
 
     private fun checkPermission() {
         if (ContextCompat.checkSelfPermission(this@MapsActivity,
@@ -141,16 +217,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                             PackageManager.PERMISSION_GRANTED)) {
                         Toast.makeText(
                             this,
-                            "Permission Granted",
+                            "Permissão de localização concedida",
                             Toast.LENGTH_SHORT
                         ).show()
                     }
                 } else {
-                    Toast.makeText(
-                        this,
-                        "Permission Denied",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    Toast.makeText(this,
+                        "Permissão negada. Para utilizar o aplicativo, " +
+                            "permita o acesso a localização",
+                        Toast.LENGTH_LONG).show()
                 }
                 return
             }
